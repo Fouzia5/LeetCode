a7d2b84d0c1db35b069a8b00f373c67d2344b650
Fouzia Akbar <bc250406680fak@vu.edu.pk>
2026-01-03 22:04:38 +0500

    Time: 7 ms (83.84%) | Memory: 17.3 MB (96.04%) - LeetSync

--- 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.py
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.py
@@ -1,10 +1,8 @@
 class Solution:
     def numOfWays(self, n: int) -> int:
         MOD = 10**9 + 7
-        
-        # Base case for n = 1
-        abc = 6  # all different
-        aba = 6  # first and third same
+        abc = 6  
+        aba = 6  
         
         for _ in range(2, n + 1):
             new_abc = (abc * 2 + aba * 2) % MOD
