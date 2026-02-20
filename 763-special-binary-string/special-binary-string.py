ea724fff2cf55d278224d32cf1f98f4a4ad02cbd
Fouzia Akbar <bc250406680fak@vu.edu.pk>
2026-02-20 21:54:01 +0500

    Time: 0 ms (100.00%) | Memory: 12.5 MB (66.13%) - LeetSync

---
+++ 763-special-binary-string/special-binary-string.py
@@ -0,0 +1,22 @@
+class Solution:
+    def makeLargestSpecial(self, s):
+        if len(s) <= 2:
+            return s
+        
+        count = 0
+        start = 0
+        blocks = []
+        
+        for i in range(len(s)):
+            if s[i] == '1':
+                count += 1
+            else:
+                count -= 1
+            
+            if count == 0:
+                inner = self.makeLargestSpecial(s[start+1:i])
+                blocks.append("1" + inner + "0")
+                start = i + 1
+        
+        blocks.sort(reverse=True)
+        return "".join(blocks)
